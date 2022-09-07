a824621ab6a39d681dbac9f9826a3eca3935d12d
Ainsik <pio.andrzejczuk@gmail.com>
2022-09-08 00:50:25 +0200

    testing different way

--- static/js/actors.js
+++ static/js/actors.js
@@ -1,9 +1,12 @@
-const all_actors = document.querySelectorAll(".hiden");
 const all_actors_btn = document.querySelectorAll(".name");
 
 all_actors_btn.forEach((actor_btn) => {
 	actor_btn.addEventListener("click", (e) => {
 		const div = e.target.querySelector(".hiden");
-		div.toggleAttribute("hidden");
+		// div.toggleAttribute("hidden");
+		const result = div.hasAttribute("hidden");
+		result
+			? div.removeAttribute("hidden")
+			: div.setAttribute("hidden", "hidden");
 	});
 });
